0c738470d7d4ff4daf1ca2a6ac60ca059280d15d
susu <28025920@qq.com>
2016-04-24 21:33:22 +0800

    完成心跳动画效果

--- EffectOfTheHeart/EffectOfTheHeart/ViewController.m
+++ EffectOfTheHeart/EffectOfTheHeart/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIImageView *imageV;
 
 @end
 
@@ -16,6 +17,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //创建动画
+    CABasicAnimation* anim=[CABasicAnimation animation];
+    
+    //设置描述哪个属性产生动画
+    anim.keyPath=@"transform.scale";
+    
+    //设置值
+    anim.toValue=@0.5;
+    
+    //设置动画次数
+    anim.repeatCount=MAXFLOAT;
+    
+    //取消反弹效果
+    anim.removedOnCompletion=NO;
+    
+    //设置动画执行后保持最新效果
+    anim.fillMode=kCAFillModeForwards;
+    
+    [_imageV.layer addAnimation:anim forKey:nil];
+    
     
 }
 
